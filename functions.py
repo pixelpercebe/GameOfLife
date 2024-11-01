fcfbb7926c260a56aa0ba403cd76ba2db5d91693
Percebe <83479307+pixelpercebe@users.noreply.github.com>
2024-11-01 02:29:35 +0100

    game logic perfectly defined, todo: create an ui with the controls of the game

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy
+import copy
 
 
 def create_grid(dimensions: tuple, x_size, y_size):
@@ -6,22 +7,56 @@ def create_grid(dimensions: tuple, x_size, y_size):
     col = dimensions[0] // x_size
     row = dimensions[1] // y_size
     matrix = numpy.zeros((row, col))
-    print(row, col)
-    print(len(matrix)-1,len(matrix[1])-2)
     return matrix
 
 
 def analyse_neighbours(matrix, population):
-    ##population = (row,col)
-    neigh_count_matrix = numpy.zeros((len(matrix),len(matrix)))
+    # population = (row,col)
+    neigh_count_matrix = copy.deepcopy(matrix)
+    for row in neigh_count_matrix:
+        for col in range(len(row)):
+            row[col] = 0
     for neig in population:
         for row in range(neig[0]-1 if neig[0]-1 >= 0 else neig[0],
                          neig[0]+2 if neig[0]+1 < len(matrix) else neig[0]+1):
             for col in range(neig[1]-1 if neig[1]-1 >= 0 else neig[1],
                              neig[1]+2 if neig[1]+1 < len(matrix[1]) else neig[1]+1):
-                if (row,col) == neig:
+                if (row, col) == neig:
                     continue
-                ##if matrix[row][col] == 1:
-                    ##neigh_count_matrix[neig[0]][neig[1]] += 1
-                matrix[row][col] = 2
+                if matrix[row][col] == 1:
+                    neigh_count_matrix[neig[0]][neig[1]] += 1
+                else:
+                    neigh_count_matrix[row][col] += 1
+    return neigh_count_matrix
 
+
+def verify_alive(old_matrix, neighbour_count_matrix):
+    new_matrix = copy.deepcopy(neighbour_count_matrix)
+    for row in new_matrix:
+        for col in range(len(row)):
+            row[col] = 0
+    for row in range(len(neighbour_count_matrix)):
+        for col in range(len(neighbour_count_matrix[row])):
+            n = neighbour_count_matrix[row][col]
+            current_state = old_matrix[row][col]
+            match n:
+                case _ if n < 2 or n > 3:
+                    new_matrix[row][col] = 0
+                case _ if current_state == 1 and n == 2:
+                    new_matrix[row][col] = 1
+                case _ if n == 3:
+                    new_matrix[row][col] = 1
+    return new_matrix
+
+
+def clean_matrix(matrix):
+    for row in matrix:
+        for col in range(len(row)):
+            row[col] = 0
+    return matrix
+
+
+def print_matrix(matrix):
+    print("new iteration")
+    for i in matrix:
+        print(i)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Example file showing a basic pygame "game loop"
-import numpy
 import pygame
 import functions
 
@@ -8,10 +7,10 @@ def draw_grid():
     aux_x_size = x_size
     aux_y_size = y_size
     for row in range(len(matrix)):
-        pygame.draw.line(screen, "grey", (0, aux_x_size), (dimensions[0], aux_x_size),1)
+        pygame.draw.line(screen, "white", (0, aux_x_size), (dimensions[0], aux_x_size), 1)
         aux_x_size = aux_x_size + x_size
         for col in range(len(matrix[0])):
-            pygame.draw.line(screen, "grey", (aux_y_size, 0), (aux_y_size, dimensions[1]), 1)
+            pygame.draw.line(screen, "white", (aux_y_size, 0), (aux_y_size, dimensions[1]), 1)
             aux_y_size = aux_y_size + x_size
 
 
@@ -23,19 +22,24 @@ def draw_population():
             draw_pix_x = row * x_size
             draw_pix_y = col * y_size
             if matrix[row][col] == 1:
-                init_population.append((row,col))
-                pygame.draw.rect(screen,"white",(draw_pix_y,draw_pix_x,x_size,y_size))
-            if matrix[row][col] == 2:
-                pygame.draw.rect(screen, "blue", (draw_pix_y, draw_pix_x, x_size, y_size))
+                init_population.append((row, col))
+                pygame.draw.rect(screen, "white", (draw_pix_y, draw_pix_x, x_size, y_size))
     return init_population
 
+
 def analyse(population):
-    functions.analyse_neighbours(matrix, population)
+    count_matrix = functions.analyse_neighbours(matrix, population)
+    new_matrix = functions.verify_alive(matrix, count_matrix)
+    return new_matrix
+
+
+def clean_screen(current_matrix):
+    functions.clean_matrix(current_matrix)
+    draw_population()
 
 
 # pygame setup
 pygame.init()
-
 start = 0
 dimensions = (1280, 720)  # 1280,720
 x_size = 10
@@ -50,32 +54,35 @@ screen.fill("black")
 matrix = functions.create_grid(dimensions, x_size, y_size)
 
 while running:
-
-    ##draw_grid()  ##dibuja la matriz si es necesario
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    pop = draw_population()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print("mousebuttondown")
-            mouse_pos = pygame.mouse.get_pos();
+            mouse_pos = pygame.mouse.get_pos()
             if matrix[mouse_pos[1]//x_size][mouse_pos[0]//y_size] == 0:
                 matrix[mouse_pos[1]//x_size][mouse_pos[0]//y_size] = 1
             else:
                 matrix[mouse_pos[1] // x_size][mouse_pos[0] // y_size] = 0
-            pop=draw_population()
         if event.type == pygame.KEYDOWN:
-            if event.key & pygame.K_SPACE:
-                start = 1 if start == 0 else 0
-                print(start)
-                analyse(pop)
-
-
-
+            match event.key:
+                case pygame.K_SPACE:
+                    start = 1 if start == 0 else 0
+                    print(start)
+                case pygame.K_w:
+                    start = 0
+                    matrix = analyse(pop)
+                    print("next iteration")
+                case pygame.K_ESCAPE:
+                    start = 0
+                    clean_screen(matrix)
+    if start:
+        matrix = analyse(pop)
 
     # RENDER YOUR GAME HERE
     # flip() the display to put your work on screen
     pygame.display.flip()
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(10)  # limits FPS to 60
 pygame.quit()
